0903c4addf39c333172755cd8e692ce6e48b89e0
wen3x <egor44gamer@gmail.com>
2026-08-02 14:49:11 +0300

    Added model 'SiteConfig'

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,28 @@
+from django import forms
 from django.contrib import admin
+from django_svg_image_form_field import SvgAndImageFormField
+from .models import SiteConfig
 
-# Register your models here.
+class SiteConfigForm(forms.ModelForm):
+    LOGO = SvgAndImageFormField(required=False, label="Logo (SVG)")
+    FAVICON = SvgAndImageFormField(required=False, label="Favicon (SVG)")
+
+    class Meta:
+        model = SiteConfig
+        fields = '__all__'
+        widgets = {
+            'ACCENT_COLOR': forms.TextInput(attrs={'type': 'color', 'style': 'height: 40px; width: 100px;'}),
+            'BACKGROUND_COLOR': forms.TextInput(attrs={'type': 'color', 'style': 'height: 40px; width: 100px;'}),
+        }
+
+@admin.register(SiteConfig)
+class SiteConfigAdmin(admin.ModelAdmin):
+    form = SiteConfigForm
+
+    def has_add_permission(self, request):
+        if SiteConfig.objects.exists():
+            return False
+        return super().has_add_permission(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- core/models.py
+++ core/models.py
@@ -1,1 +1,55 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
+
+class SiteConfig(models.Model):
+    FONT_FAMILY_CHOICES = [
+        ('roboto', 'Roboto (Sans-serif)'),
+        ('inter', 'Inter (Modern)'),
+        ('pt-serif', 'PT Serif (Serif)'),
+        ('monospace', 'Monospace (Code)'),
+    ]
+
+    SITE_NAME = models.CharField(max_length=20, default="MintBoard")
+    
+    ACCENT_COLOR = models.CharField(
+        max_length=7, 
+        default="#2ECC71", 
+        verbose_name="Accent color (HEX)"
+    )
+    
+    BACKGROUND_COLOR = models.CharField(
+        max_length=7, 
+        default="#ffffff", 
+        verbose_name="Background color (HEX)"
+    )
+    
+    LOGO = models.FileField(
+        upload_to='site/logo/',
+        validators=[FileExtensionValidator(allowed_extensions=['svg'])],
+        verbose_name="Logo (SVG)",
+        blank=True,
+        null=True
+    )
+    
+    FAVICON = models.FileField(
+        upload_to='site/favicon/',
+        validators=[
+            FileExtensionValidator(allowed_extensions=['svg']), 
+        ],
+        verbose_name="Favicon (SVG)",
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = "Site Settings"
+        verbose_name_plural = "Site Settings"
+
+    def save(self, *args, **kwargs):
+        if not self.pk and SiteConfig.objects.exists():
+            raise ValidationError("One copy of this model only.")
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return "Site Settings"
